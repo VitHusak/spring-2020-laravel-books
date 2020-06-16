4fc4dcde74d886a37a15d8f94a67cc61918ad18d
j_polak <jan.polak@onmyway.cz>
2020-06-16 14:05:22 +0200

    seeding db with books, adding columns

---
+++ database/seeds/BooksSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = json_decode( file_get_contents( storage_path('books.json') ) );
+
+        dd($data);
+    }
+}
