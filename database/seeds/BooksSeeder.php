fd4b39b111fddce4c72a11580021311cf3ac02e0
j_polak <jan.polak@onmyway.cz>
2020-06-16 14:38:28 +0200

    saving authors in BooksSeeder

--- database/seeds/BooksSeeder.php
+++ database/seeds/BooksSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Author;
+use App\Book;
+use App\Publisher;
 
 class BooksSeeder extends Seeder
 {
@@ -13,6 +16,32 @@ class BooksSeeder extends Seeder
     {
         $data = json_decode( file_get_contents( storage_path('books.json') ) );
 
-        dd($data);
+        // insert all the authors into db
+        $authors = [];
+        foreach ($data as $book) {
+            $authors[] = $book->author; // add author name into array $authors
+        }
+
+        $unique_authors = array_unique($authors);
+
+        foreach ($unique_authors as $author) {
+            $new_author = new Author;
+            $new_author->name = $author;
+            $new_author->save();
+        }
+
+        // insert all the publishers
+
+        // prepare a list of all authors that we can grab authors from by their names
+
+        // prepare a list of all publishers that we can grab publishers from by their names
+
+        foreach ($data as $book) {
+            // insert a record into the books table
+
+            // grab the id of an author from the list prepared before
+
+            // grab the id of an publisher from the list prepared before
+        }
     }
 }
