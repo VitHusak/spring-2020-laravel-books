451f8365a1402337ad3975f4c6356492de90f695
j_polak <jan.polak@onmyway.cz>
2020-06-16 15:41:07 +0200

    BooksSeeder update

--- database/seeds/BooksSeeder.php
+++ database/seeds/BooksSeeder.php
@@ -15,6 +15,11 @@ class BooksSeeder extends Seeder
     public function run()
     {
         $data = json_decode( file_get_contents( storage_path('books.json') ) );
+dd($data);
+        // empty the tables
+        \DB::statement('TRUNCATE TABLE `books`');
+        \DB::statement('TRUNCATE TABLE `authors`');
+        \DB::statement('TRUNCATE TABLE `publishers`');
 
         // insert all the authors into db
         $authors = [];
@@ -29,19 +34,45 @@ class BooksSeeder extends Seeder
             $new_author->name = $author;
             $new_author->save();
         }
-
+        
         // insert all the publishers
+        $publishers = [];
+        foreach ($data as $book) {
+            $publishers[] = $book->publisher; // add author name into array $authors
+        }
+
+        $unique_publishers = array_unique($publishers);
+
+        foreach ($unique_publishers as $publisher) {
+            $new_publisher = new Publisher;
+            $new_publisher->title = $publisher;
+            $new_publisher->save();
+        }
 
         // prepare a list of all authors that we can grab authors from by their names
-
+        $all_authors = Author::all()->pluck('id', 'name')->toArray();
+        
         // prepare a list of all publishers that we can grab publishers from by their names
+        $all_publishers = Publisher::all()->pluck('id', 'title')->toArray();
 
         foreach ($data as $book) {
-            // insert a record into the books table
 
-            // grab the id of an author from the list prepared before
+            // insert a record into the books table
+            $new_book = new Book;
+            $new_book->title   = $book->title;
+            $new_book->price   = $book->price;
+            $new_book->image   = $book->image;
+            $new_book->authors = $book->author;
+            $new_book->publication_date = $book->{"publication-date"};
 
             // grab the id of an publisher from the list prepared before
+            $new_book->publisher_id = $all_publishers[ $book->publisher ];
+
+            // save the book (so that we know its id for the table author_book)
+            $new_book->save();
+
+            // grab the id of an author from the list prepared before
+            $author_id = $all_authors[ $book->author ];
         }
     }
 }
